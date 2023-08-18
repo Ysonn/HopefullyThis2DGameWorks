7730deb669b19cf63aaf16bb45c762550fcd1de6
Ysonn <132964181+Ysonn@users.noreply.github.com>
2023-08-19 11:13:03 +1200

    dsffh

--- 2D_Towers_Version/Assets/Scripts/BlueCannonManager.cs
+++ 2D_Towers_Version/Assets/Scripts/BlueCannonManager.cs
@@ -8,22 +8,23 @@ public class BlueCannonManager: MonoBehaviour
     public GameObject blueBarrelEnd;
     public float speed = 1;
     public float RotAngleY = 45;
-    public SHOOTTHECANNON SHOOTTHECANNON; // Reference to the AimBarrel script
 
+    
     // Update is called once per frame
     void Update()
     {
-        if (SHOOTTHECANNON.personShooting % 2 == 0)
+        
+        if (TurnManagementScript.turnNumber % 2 == 0)
         {
             float rotationZ = Mathf.SmoothStep(-45, RotAngleY, Mathf.PingPong(Time.time * speed, 1));
             transform.rotation = Quaternion.Euler(0, 180, rotationZ);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && (SHOOTTHECANNON.personShooting % 2 == 0))
+        if (TurnManagementScript.turnNumber % 2 == 0)
         {
             Instantiate(cannonBall, blueBarrelEnd.transform.position, transform.rotation);
             Debug.Log("Blue shot first!");
-            SHOOTTHECANNON.personShooting += 1;
+            TurnManagementScript.turnNumber += 1;
         }
     }
 }
--- 2D_Towers_Version/Assets/Scripts/RedCannonManager.cs
+++ 2D_Towers_Version/Assets/Scripts/RedCannonManager.cs
@@ -8,23 +8,22 @@ public class RedCannonManager : MonoBehaviour
     public GameObject redBarrelEnd;
     public float speed = 1;
     public float RotAngleY = 45;
-    public SHOOTTHECANNON SHOOTTHECANNON; // Reference to the AimBarrel script
+  
 
     // Update is called once per frame
     void Update()
     {
-        if (SHOOTTHECANNON.personShooting % 2 == 1)
+        if (TurnManagementScript.turnNumber % 2 == 1)
         {
             float rotationZ = Mathf.SmoothStep(-45, RotAngleY, Mathf.PingPong(Time.time * speed, 1));
             transform.rotation = Quaternion.Euler(0, 0, rotationZ);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && (SHOOTTHECANNON.personShooting % 2 == 1))
+        if (TurnManagementScript.turnNumber % 2 == 1)
         {
             Instantiate(cannonBall, redBarrelEnd.transform.position, transform.rotation);
             Debug.Log("Red shot first!");
-            SHOOTTHECANNON.personShooting += 1;
-        
+            TurnManagementScript.turnNumber += 1;
         }
     }  
 }
