0bfd198f82b9e5277f917828edeccfdd49824bf9
Ysonn <132964181+Ysonn@users.noreply.github.com>
2023-08-20 22:40:48 +1200

    fsdgh

--- 2D_Towers_Version/Assets/Scripts/TNTManager.cs
+++ 2D_Towers_Version/Assets/Scripts/TNTManager.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class TNTManager : MonoBehaviour
 {
+    
+    public AudioClip FuseHissing;
+    public AudioClip Explosion;
+    private AudioSource audioSource;
+    private Rigidbody2D TntRigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = 1f;
+        audioSource.clip = FuseHissing;
+        audioSource.Play();
+        TntRigidBody = GetComponent<Rigidbody2D>();
+        Vector2 force = transform.right * 5f;
+        TntRigidBody.AddForce(force, ForceMode2D.Impulse);
+        TntRigidBody.AddTorque(5.0f, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        audioSource.Stop();
+        audioSource.volume = 1f;
+        audioSource.PlayOneShot(Explosion);
     }
 }
