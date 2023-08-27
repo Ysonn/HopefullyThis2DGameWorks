8752b5326bf045cc63d34b42e0883bde0219ada7
Ysonn <132964181+Ysonn@users.noreply.github.com>
2023-08-27 16:21:50 +1200

    rdtfyghu

--- 2D_Towers_Version/Assets/Scripts/TutorialManager.cs
+++ 2D_Towers_Version/Assets/Scripts/TutorialManager.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 
 public class TutorialManager : MonoBehaviour
 {
-    public Image[] tutorialImagesEnglish; 
-    public Image[] tutorialImagesSwedish; 
+    public GameObject[] tutorialImagesEnglish; 
+    public GameObject[] tutorialImagesSwedish; 
     private int currentImageEnglish = 0; 
     private int currentImageSwedish = 0; 
     // Start is called before the first frame update
