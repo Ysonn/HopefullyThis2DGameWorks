8440db8f444d7361e842581131f8c6ddfb44e18e
Ysonn <132964181+Ysonn@users.noreply.github.com>
2023-08-28 13:17:56 +1200

    dsfrrgsd

--- 2D_Towers_Version/Assets/Scripts/BlueKingManager.cs
+++ 2D_Towers_Version/Assets/Scripts/BlueKingManager.cs
@@ -55,7 +55,7 @@ public class BlueKingManager : MonoBehaviour
     IEnumerator DelayedChangeScene()
     {
         // Wait for 5 seconds
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(4.0f);
 
         // Change to the "MainMenu" scene
         SceneManager.LoadScene("MainMenu");
--- 2D_Towers_Version/Assets/Scripts/RedKingManager.cs
+++ 2D_Towers_Version/Assets/Scripts/RedKingManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class RedKingManager : MonoBehaviour
 {
     public Sprite DeadRedKing;
-    public AudioClip KingDying;
+    public AudioClip kingDying;
     public AudioClip blueWins;
     public static bool redLost = false;
     private AudioSource audioSource;
@@ -40,7 +40,7 @@ public class RedKingManager : MonoBehaviour
     private void Die()
     {
         //play the win and king dying sounds
-        audioSource.PlayOneShot(KingDying);
+        audioSource.PlayOneShot(kingDying);
         audioSource.PlayOneShot(blueWins);
         //change the king to look dead 
         spriteRenderer.sprite = DeadRedKing;
@@ -52,7 +52,7 @@ public class RedKingManager : MonoBehaviour
     IEnumerator DelayedChangeScene()
     {
         // Wait for 3 seconds
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(4.0f);
 
         // Change to the "MainMenu" scene
         SceneManager.LoadScene("MainMenu");
